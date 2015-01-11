32d372bbc5b1b477a08eb8d9e9579b5f388fdaaf
PavelNPavlov <pavelnapavlov@gmail.com>
2015-01-11 14:00:55 +0200

    Age1

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿/*
+Problem 15.* Age after 10 Year
+Write a program to read your birthday from the console and print how old you are now and how old you will be after 10 years.
+ */
+using System;
 class Age10
 {
     static void Main()
